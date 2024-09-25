c330b1e1d596740dfecb715f89d11c4e12b10214
Khushavant <khushawantwagh2002@gmail.com>
2024-09-25 09:17:00 +0530

    add

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 function Background() {
   return (
     <>
-      {/* <div className='fixed z-[2] w-full h-screen'> */}
-      <div className="fixed z-[2] w-full h-screen bg-[url('./public/khush2.jpg')] bg-cover bg-center">
+      <div className='fixed z-[2] w-full h-screen'>
 
-        <div className='absolute top-[5%] w-full py-10 flex justify-center text-white text-xl font-semibold hover:text-yellow-500 transition-colors duration-300 ease-in-out'>
+
+        <div className='absolute top-[5%] w-full py-10 flex justify-center text-black text-xl font-semibold hover:text-yellow-500 transition-colors duration-300 ease-in-out'>
           Documents
         </div>
-        <h1 className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[12vw] leading-none tracking-tighter font-semibold hover:text-yellow-500 transition-colors duration-300 ease-in-out text-yellow-500'>
+        <h1 className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[12vw] leading-none tracking-tighter font-semibold hover:text-yellow-500 transition-colors duration-300 ease-in-out'>
           Docs
         </h1>
       </div>
